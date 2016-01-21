7f8d8090d16f5320516bb0c1b7b08a5b43352e7f
allanak <allan@allank.com>
2016-01-20 22:01:03 -0800

    Update onkyoIP.groovy

--- onkyoIP.groovy
+++ onkyoIP.groovy
@@ -1,6 +1,6 @@
 /**
- *  Onkyo IP Control
- *	Allan Klein
+ * Onkyo IP Control
+ *  Allan Klein (@allanak)
  *  Originally based on: Mike Maxwell's code
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
